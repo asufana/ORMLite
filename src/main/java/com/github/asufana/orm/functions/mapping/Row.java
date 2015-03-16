9c2594dcdb4bb4bcfbb1cfce0dc02fee8e3f5a7e
hana <asufanah@gmail.com>
2015-03-16 23:20:30 +0900

    Extract PK function on Row.java to PrimaryKeyFunction.java

--- src/main/java/com/github/asufana/orm/functions/mapping/Row.java
+++ src/main/java/com/github/asufana/orm/functions/mapping/Row.java
@@ -1,16 +1,13 @@
 package com.github.asufana.orm.functions.mapping;
 
-import java.lang.reflect.*;
 import java.util.*;
 import java.util.function.*;
 
 import lombok.*;
 
 import com.github.asufana.orm.*;
-import com.github.asufana.orm.exceptions.*;
-import com.github.asufana.orm.functions.inspection.*;
-import com.github.asufana.orm.functions.inspection.resources.*;
 import com.github.asufana.orm.functions.query.*;
+import com.github.asufana.orm.functions.util.*;
 
 @Getter
 public class Row<T> {
@@ -44,47 +41,13 @@ public class Row<T> {
     //-----------------------------------------
     
     public Integer delete() {
+        final String pkColumnName = PrimaryKeyFunction.pkColumnName(em.connection(),
+                                                                    em.tableName());
         return Query.execute(em.connection(),
                              String.format("DELETE FROM %s WHERE %s=?",
                                            em.tableName(),
-                                           pkColumnName()),
-                             Arrays.asList(pkFieldValue()));
-    }
-    
-    private String pkColumnName() {
-        final ColumnList pkColumns = Inspection.pkColumns(em.connection(),
-                                                          em.tableName());
-        assert pkColumns.size() == 1 : "複合主キーにはまだ未対応...";
-        return pkColumns.get(0).get().columnName();
-    }
-    
-    private Object pkFieldValue() {
-        try {
-            final String pkColumnName = pkColumnName();
-            final Field pkField = Arrays.asList(em.targetClass()
-                                                  .getDeclaredFields())
-                                        .stream()
-                                        .filter(f -> f.getName()
-                                                      .toLowerCase()
-                                                      .equals(pkColumnName.toLowerCase()))
-                                        .findFirst()
-                                        .orElseThrow(() -> new ORMLiteException(String.format("Can't find PK Field on Target Class. Target Class:%s, PK Field:%s",
-                                                                                              em.targetClass()
-                                                                                                .getSimpleName(),
-                                                                                              pkColumnName)));
-            pkField.setAccessible(true);
-            final Object pkFieldValue = pkField.get(instance);
-            if (pkFieldValue == null) {
-                throw new ORMLiteException(String.format("Can't get PK Field value on Target Class. Target Class:%s, PK Field:%s",
-                                                         em.targetClass()
-                                                           .getSimpleName(),
-                                                         pkField.getName()));
-            }
-            return pkFieldValue;
-        }
-        catch (IllegalArgumentException | IllegalAccessException
-                | SecurityException e) {
-            throw new ORMLiteException(e);
-        }
+                                           pkColumnName),
+                             Arrays.asList(PrimaryKeyFunction.pkFieldValue(pkColumnName,
+                                                                           instance)));
     }
 }
